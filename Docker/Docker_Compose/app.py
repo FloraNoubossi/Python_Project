a3b3bdcb5bc84de1689b4d4cae61d3d41b48d705
floranoubossi <i.magnenoubossi@h3hitema.fr>
2024-09-25 20:29:04 +0200

    Docker and HTTPS

--- Docker/Docker_Compose/app.py
+++ Docker/Docker_Compose/app.py
@@ -7,7 +7,10 @@ redis = Redis(host='redis-container', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'This page has been viewed {} time(s).'.format(redis.get('hits'))
+    # Décoder la valeur renvoyée par Redis
+    hits = redis.get('hits').decode('utf-8')
+    return f'This page has been viewed {hits} time(s).'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
